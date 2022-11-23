a4ddb683f35c4d9accf3e5021240b9408c353ecd
whatasame <whatasame@gmail.com>
2022-11-23 23:56:01 +0900

    테스트용 클래스 추가

--- src/test/HttpClientTestApp.java
+++ src/test/HttpClientTestApp.java
@@ -1,5 +1,6 @@
 package test;
 
+<<<<<<< HEAD
 import org.json.JSONObject;
 
 import java.io.*;
@@ -9,10 +10,17 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 import java.util.function.Function;
+=======
+import java.io.*;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.Properties;
+>>>>>>> 935d834 (테스트용 클래스 추가)
 
 public class HttpClientTestApp {
 
     public static void main(String[] args) {
+<<<<<<< HEAD
 
         /* GET TEST */
         HttpRequest testRequest = new HttpRequest("GET", "/httpTest", "HTTP/1.1", null);
@@ -41,6 +49,13 @@ public class HttpClientTestApp {
     }
 
     public static HttpResponse requestToServer(HttpRequest httpRequest) {
+=======
+        httpRequest("/httpTestd");
+    }
+
+
+    public static void httpRequest(String path) {
+>>>>>>> 935d834 (테스트용 클래스 추가)
 
         // 서버 정보
         String SERVER_IP;
@@ -49,11 +64,15 @@ public class HttpClientTestApp {
         try {
             properties.load(new FileInputStream(".properties"));
             SERVER_IP = properties.getProperty("SERVER_IP");
+<<<<<<< HEAD
 //            SERVER_IP = "localhost";
+=======
+>>>>>>> 935d834 (테스트용 클래스 추가)
             SERVER_PORT = Integer.parseInt(properties.getProperty("SERVER_PORT"));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+<<<<<<< HEAD
 
         // HTTP 통신
         try {
@@ -72,21 +91,64 @@ public class HttpClientTestApp {
 
             return httpResponse;
 
+=======
+        
+        // HTTP 통신
+        try {
+            Socket socket = new Socket(SERVER_IP, SERVER_PORT); // 서버 연결
+
+            request(socket.getOutputStream(), path); // 서버에 요청
+            response(socket.getInputStream()); // 서버의 응답 처리
+
+            socket.close();
+>>>>>>> 935d834 (테스트용 클래스 추가)
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
+<<<<<<< HEAD
     private static HttpResponse responseBuilder(InputStream inputStream) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
 
         // HTTP 패킷 수신
         StringBuilder stringBuilder = new StringBuilder();
         String inputLine;
+=======
+    public static void request(OutputStream outputStream, String path) {
+
+        StringBuilder stringBuilder = new StringBuilder();
+        String body = "안녕하세요?";
+
+        // header 제작
+        stringBuilder.append("GET ").append(path).append(" HTTP/1.1\\r\\n");
+        stringBuilder.append("Content-Length: ").append(body.getBytes().length).append("\r\n");
+        stringBuilder.append("\r\n");
+
+        // body 제작
+        stringBuilder.append(body);
+
+        try {
+            outputStream.write(stringBuilder.toString().getBytes(StandardCharsets.UTF_8));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    public static void response(InputStream inputStream) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+
+        StringBuilder stringBuilder = new StringBuilder();
+        String inputLine;
+
+        // HTTP 패킷 수신
+>>>>>>> 935d834 (테스트용 클래스 추가)
         while ((inputLine = bufferedReader.readLine()) != null) {
             stringBuilder.append(inputLine).append("\r\n");
         }
 
+<<<<<<< HEAD
         // HTTP 패킷 parse
         String response = stringBuilder.toString();
         String[] responseArr = response.split("\r\n");
@@ -195,3 +257,8 @@ class RequestController {
     };
 
 }
+=======
+        System.out.println(stringBuilder.toString());
+    }
+}
+>>>>>>> 935d834 (테스트용 클래스 추가)
