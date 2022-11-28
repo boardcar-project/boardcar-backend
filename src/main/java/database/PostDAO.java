2640ca2a6d05b712625fecd21d416b0690ce7029
whatasame <whatasame@gmail.com>
2022-11-28 22:49:03 +0900

    ORA-01861 버그 수정 (to_date 누락 추가)

--- src/main/java/database/PostDAO.java
+++ src/main/java/database/PostDAO.java
@@ -18,20 +18,13 @@ public class PostDAO {
         String TYPE = jsonObject.getString("TYPE");
 
         // SQL query 생성
-//        PreparedStatement sqlQuery = HttpServer.getDatabaseConnection().prepareStatement("INSERT INTO POST (MID, PDATE, TITLE, BODY, TYPE) VALUES (?,?,?,?,?)");
-        PreparedStatement sqlQuery = HttpServer.getDatabaseConnection().prepareStatement("INSERT INTO POST (PID, MID, PDATE, TITLE, BODY, TYPE) VALUES (PCOUNTER.nextval, ?,?,?,?,?)");
-
-
+        PreparedStatement sqlQuery = HttpServer.getDatabaseConnection().prepareStatement("INSERT INTO POST (PID, MID, PDATE, TITLE, BODY, TYPE) VALUES (PCOUNTER.nextval, ?,to_date(?, 'YYYY-MM-DD'),?,?,?)");
 
         sqlQuery.setString(1, MID);
         sqlQuery.setString(2, PDATE);
         sqlQuery.setString(3, TITLE);
         sqlQuery.setString(4, BODY);
         sqlQuery.setString(5, TYPE);
-
-//        System.out.println("hi");
-//        PreparedStatement sqlQuery = HttpServer.getDatabaseConnection().prepareStatement("INSERT INTO POST (PID, MID, PDATE, TITLE, BODY, TYPE) VALUES (PCOUNTER.nextval, 'testid','2022-11-11','testTitle','testBody','자유')");
-
 
         return sqlQuery.executeUpdate(); // SQL 실행 후 업데이트 된 레코드 수 반환
     }
