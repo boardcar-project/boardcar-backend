522b8fd2b00cbffe81354314cf0b2067d5785cb7
whatasame <whatasame@gmail.com>
2022-11-30 00:29:39 +0900

    누락 column 추가

--- src/main/java/database/MemberVO.java
+++ src/main/java/database/MemberVO.java
@@ -10,12 +10,16 @@ public class MemberVO {
     private final String MID;
     private final String PASSWORD;
     private final String NAME;
+    private final boolean IS_ADMIN;
+    private final int CID;
     private final String EMAIL;
 
     public MemberVO(ResultSet resultSet) throws SQLException {
         this.MID = resultSet.getString("MID");
         this.PASSWORD = resultSet.getString("PASSWORD");
         this.NAME = resultSet.getString("NAME");
+        this.IS_ADMIN = resultSet.getBoolean("IS_ADMIN");
+        this.CID = resultSet.getInt("CID");
         this.EMAIL = resultSet.getString("EMAIL");
     }
 
@@ -25,6 +29,8 @@ public class MemberVO {
         jsonObject.put("MID", MID);
         jsonObject.put("PASSWORD", PASSWORD);
         jsonObject.put("NAME", NAME);
+        jsonObject.put("IS_ADMIN", IS_ADMIN);
+        jsonObject.put("CID", CID);
         jsonObject.put("EMAIL", EMAIL);
 
         return jsonObject.toString();
